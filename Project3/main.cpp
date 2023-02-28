d5763bd82becb5f6cc5add9856a5d81bd5cd8ed0
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 22:06:36 +0800

    double compare

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,10 +1,12 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	int a = 0;
-	int b = 0;
+	double a = 0;
+	double b = 0;
 	while (cin >> a >> b) {
-		cout << "the smaller is:" << ((a < b) ? a : b) << " the larger is" << ((a < b) ? b : a);
+		if (a-b<0.01&&b-a<0.01) cout << "numbers are almost equal\n";
+		else if (a < b) cout << "the smaller is:" << a << "the larger is:" << b << "\n";
+		else  cout << "the smaller is:" << b << "the larger is:" << a << "\n";
 	}
 	return 0;
 }
