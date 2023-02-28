f1f0ea33af2e7651370fe62f03f82ab3ff83b8f4
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 22:13:58 +0800

    largest so far

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,12 +1,13 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	double a = 0;
-	double b = 0;
-	while (cin >> a >> b) {
-		if (a-b<0.01&&b-a<0.01) cout << "numbers are almost equal\n";
-		else if (a < b) cout << "the smaller is:" << a << "the larger is:" << b << "\n";
-		else  cout << "the smaller is:" << b << "the larger is:" << a << "\n";
+	double max=-32767;
+	double min=32767;
+	double tmp = 0;
+	while (cin >> tmp) {
+		if (tmp < min) min = tmp;
+		if (tmp > max)max = tmp;
+		cout << "the smallest so far:" << min << " the largest so far:" << max << endl;
 	}
 	return 0;
 }
