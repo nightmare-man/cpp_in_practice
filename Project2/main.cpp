7dc0aeacfa759adcba35f0d6fd4a33b74290de72
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 11:03:39 +0800

    string input and operate

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,11 +1,9 @@
 #include "../include/std_lib_facilities.h"
 int main() {
-	cout << "enter a float:\n";
-	int a;
-	cin >> a;
-	cout << "a==" << a
-		<< "\na+1==" << a + 1
-		<< "\nhalf of a==" << a / 2
-		<< "\nsqrt of a==" << sqrt(a);
+	cout << "enter first and second:\n";
+	string first, second;
+	cin >> first >> second;
+	string name = first +" " + second;
+	cout << "ur name is " << name;
 	return 0;
 }
