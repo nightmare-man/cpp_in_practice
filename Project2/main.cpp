d746102eb983636e8f7e0549d632e391d5b21dd1
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 11:07:02 +0800

    string declaration and definition

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,9 +1,12 @@
 #include "../include/std_lib_facilities.h"
 int main() {
-	cout << "enter first and second:\n";
-	string first, second;
-	cin >> first >> second;
-	string name = first +" " + second;
-	cout << "ur name is " << name;
+	string previous = "";
+	string current;
+	while (cin >> current) {
+		if (current == previous) {
+			cout << "repeated word:" << current << "\n";
+		}
+		previous = current;
+	}
 	return 0;
 }
