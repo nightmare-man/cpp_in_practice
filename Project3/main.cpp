40309eca05279ff355713745d25f422246fd80b3
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 18:33:00 +0800

    vector and iteration

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,9 +1,10 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	
-	cout << square(10);
-	return 0;
+	vector<string> week{ "w1","w2" };
+	for (int i = 0; i < week.size(); i++) {
+		cout << week[i] << endl;
+	}
 }
 int square(int x) {
 	return x * x;
