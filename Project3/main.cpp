e49b296ca4d208e2e6aa0d51c26c2ab25a422658
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 17:46:16 +0800

    auto type transfer

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,9 +1,6 @@
 #include "../include/std_lib_facilities.h"
 int main() {
-	constexpr double pi = 3.14159;
-	double span = 0;
-	cin >> span;
-	cout << "span "<<span <<" cicle's length is" << 2 * pi * span;
-	pi = 100;
+	cout << 5 / 2 << endl;
+	cout << 5.0 / 2;
 	return 0;
 }
