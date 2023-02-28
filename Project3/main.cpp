37c76a8ae5ced6205416b0ae636facf26b8a71e5
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 18:26:30 +0800

    character and ascii code

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,20 +1,9 @@
 #include "../include/std_lib_facilities.h"
 int main() {
-	constexpr double cm_per_inch = 2.54;
-	double length = 1;
-	char unit = 'a';
-	cout << "please enter a length followed by a unit\n";
-	cin >> length >> unit;
-	switch (unit)
-	{
-	case 'i':
-		cout << length << "in == " << cm_per_inch * length << "cm\n";
-		break;
-	case 'c':
-		cout << length << " cm == " << length / cm_per_inch << "inch\n";
-		break;
-	default:
-		break;
+	int i = 0;
+	while (i < 26) {
+		cout << char('a' + i) << " " << int('a' + i) << endl;
+		i++;
 	}
 	return 0;
 }
