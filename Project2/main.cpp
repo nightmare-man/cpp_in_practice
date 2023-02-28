e959a2aefdde6e4bda31dfb7049dbd4c738bb98f
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 16:20:33 +0800

    undefined value

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,12 +1,8 @@
 #include "../include/std_lib_facilities.h"
 int main() {
-	string previous = "";
-	string current;
-	while (cin >> current) {
-		if (current == previous) {
-			cout << "repeated word:" << current << "\n";
-		}
-		previous = current;
-	}
+	double a;
+	double b = a;
+	double z = b + 2.0;
+
 	return 0;
 }
