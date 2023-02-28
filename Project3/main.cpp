835c1eb2075e5f52e1f882ac5d5b41316a2a6976
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 18:42:33 +0800

    sort

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,11 +1,18 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	vector<string> week{ "w1","w2" };
-	week.push_back("nihao");
-	for (string s : week) {
+	vector<double> temps;
+	//内置类型 如果是局部变量不会初始化 全局变量会初始化
+	//而自定义类型 一定会默认初始化
+	for (double cur; cin >> cur;) {
+		temps.push_back(cur);
+	}
+	cout << "input end\n";
+	sort(temps);
+	for (double s : temps) {
 		cout << s << endl;
 	}
+	return 0;
 }
 int square(int x) {
 	return x * x;
