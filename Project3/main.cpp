9185a3efafa680b9d02182e8851290e7ae142cc9
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 17:43:02 +0800

    constexpr

---
+++ Project3/main.cpp
@@ -0,0 +1,8 @@
+#include "../include/std_lib_facilities.h"
+int main() {
+	constexpr double pi = 3.14159;
+	double span = 0;
+	cin >> span;
+	cout << "span "<<span <<" cicle's length is" << 2 * pi * span;
+	return 0;
+}
