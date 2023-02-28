2b7c867ea8a6ef09927fcf5c5247e3cda940ddef
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 18:23:18 +0800

    cm inch transfer

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,8 +1,20 @@
 #include "../include/std_lib_facilities.h"
 int main() {
-	double a = 5.0;
-	int b = int{ a };
-	//int c{ a };
-	cout << a << b;
+	constexpr double cm_per_inch = 2.54;
+	double length = 1;
+	char unit = 'a';
+	cout << "please enter a length followed by a unit\n";
+	cin >> length >> unit;
+	switch (unit)
+	{
+	case 'i':
+		cout << length << "in == " << cm_per_inch * length << "cm\n";
+		break;
+	case 'c':
+		cout << length << " cm == " << length / cm_per_inch << "inch\n";
+		break;
+	default:
+		break;
+	}
 	return 0;
 }
