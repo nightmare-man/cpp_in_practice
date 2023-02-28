5e0ceff3f31d2d4bd77a1f4909f7650238317a10
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 21:54:19 +0800

    vector<string>

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,16 +1,13 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	vector<double> temps;
-	//内置类型 如果是局部变量不会初始化 全局变量会初始化
-	//而自定义类型 一定会默认初始化
-	for (double cur; cin >> cur;) {
-		temps.push_back(cur);
+	vector<string> words;
+	for (string tmp; cin >> tmp;) {
+		words.push_back(tmp);
 	}
-	cout << "input end\n";
-	sort(temps);
-	for (double s : temps) {
-		cout << s << endl;
+	sort(words);
+	for (int i = 0; i < words.size(); i++) {
+		if (i == 0 || words[i] != words[i - 1]) cout << words[i]<<" ";
 	}
 	return 0;
 }
