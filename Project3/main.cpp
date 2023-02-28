dc972e5f91ac43be8ebe83b52f1fb2e51f793761
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 07:53:47 +0800

    m cm inch ft transfer

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -9,19 +9,27 @@ int main() {
 	double min=32767;
 	double tmp = 0;
 	string sign = "";
+	double sum = 0;
 	while (cin >> tmp>>sign) {
 		if (sign == "m") tmp *= cm_per_m;
 		else if (sign == "inch") tmp *= cm_per_inch;
 		else if (sign == "ft")  tmp *= (inch_per_ft * cm_per_inch);
+		else if (sign == "cm") tmp = tmp;
 		else {
 			cout << "undefined sign\n";
 			break;
 		}
 		cout << "now is " << tmp << "cm" << endl;
 		numbers.push_back(tmp);
+		sum += tmp;
 		if (tmp < min) min = tmp;
 		if (tmp > max)max = tmp;
 		cout << "the smallest so far:" << min << " the largest so far:" << max << endl;
+	}
+	cout << "the smallest:" << min << " the largest:" << max << " the sum:" << sum << " the number:" << numbers.size() << endl;
+	sort(numbers);
+	for (double s : numbers) {
+		cout << s << endl;
 	}
 	return 0;
 }
