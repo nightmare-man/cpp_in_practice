d7b1b60c284309ed3676e866c30e25ebd403110c
nightmareman <107lsmlsm@whut.edu.cn>
2023-02-28 10:47:57 +0800

    新建项目

---
+++ Project2/main.cpp
@@ -0,0 +1,8 @@
+#include "../include/std_lib_facilities.h"
+int main() {
+	cout << "please enter ur first name:\n";
+	string first_name;
+	cin >> first_name;
+	cout << "hello," << first_name << "!\n";
+	return 0;
+}
