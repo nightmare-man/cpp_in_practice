9178b6c4ed3f22ebdff2c1decb692a3c797e7e6c
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 15:51:20 +0800

    被调用者处理错误

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,8 +1,24 @@
 #include "../include/std_lib_facilities.h"
-int area(int lenght, int width);
+int area(int lengh, int width);
+int framed_area(int lengh, int width);
 int main() {
-	char s1 = area(100,100);
-	cout << s1;
+	int x = -1;
+	int y = 2;
+	int z = 4;
+	int area1 = area(x, y);
+	int area2 = framed_area(1, z);
+	int area3 = framed_area(y, z);
+	cout << area3;
+	double ratio = double(area1) / area3;
+	cout << ratio;
 	return 0;
 }
-
+int area(int lengh, int width) {
+	return lengh * width;
+}
+int framed_area(int length, int width) {
+	constexpr int frame_width = 2;
+	if (length - frame_width <= 0 || width - frame_width <= 0)
+		error("non-positive area() called by framed_area()");
+	return (length - 2)*( width - 2);
+}
