97fbf840110099b23897329a47a14161f97a1111
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 19:12:21 +0800

    equation

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,32 +1,22 @@
 #include "../include/std_lib_facilities.h"
-/// <summary>
-/// c>=-273.15
-/// </summary>
-/// <param name="c"></param>
-/// <returns></returns>
-double ctok(double c) {
-	constexpr double min = 273.15;
-	if (c < -min) error("wrong argument in ctok()\n");
-	double k = c + min;
-	return k;
-}
-double ktoc(double k) {
-	constexpr double min = 273.15;
-	if (k < 0) error("wrong argument in ctok()\n");
-	double c = k- min;
-	return c;
+void trySettle(double a, double b, double c, double& a1, double& a2) {
+	int flag = b * b - 4 * a * c;
+	if(flag< 0)error("bad arguments for trySettle()\n");
+	a1 = -b + sqrt(flag);
+	a2 = -b - sqrt(flag);
+	a1 /= 2 * a;
+	a2 /= 2 * a;
 }
 int main() {
-	double k = 0;
-	cin >> k;
-	double c = 0;
+	double a, b, c;
+	double a1, a2;
 	try {
-		c = ktoc(k);
+		cin >> a >> b >> c;
+		trySettle(a, b, c, a1, a2);
 	}
 	catch (exception& e) {
-		cout << "exception: " << e.what() << endl;
+		cout << "exception :" << e.what() << endl;
 	}
-	
-	cout << c << '\n';
+	cout << "answer:" << a1 << " " << a2 << endl;
 	return 0;
 }
