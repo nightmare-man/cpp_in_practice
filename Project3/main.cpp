803ed91d835c7f6a97551d9b979f85c34cbd317b
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 09:40:12 +0800

    caculator1

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,18 +1,33 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	int max = 100;
-	int min = 1;
-	int mid = 0;
-	while (max > min) {
-		mid = (max + min) / 2;
-		cout << "is ur number smaller than " << mid << " ?\n";
-		string in = "";
-		cin >> in;
-		if (in == "y") max = mid-1;
-		else if(in == "n") min = mid;
+	double source1 = 0;
+	double source2 = 0;
+	double result = 0;
+	char op = ' ';
+	cout << "enter two number and one operator + - * / %:\n";
+	cin >> source1 >> source2;
+	cin >> op;
+	switch (op)
+	{
+	case '+':
+		result = source1 + source2;
+		break;
+	case '-':
+		result = source1 - source2;
+		break;
+	case '*':
+		result = source1 * source2;
+		break;
+	case '/':
+		result = source1 / source2;
+		break;
+	case '%':
+		result = int(source1) % int(source2);
+	default:
+		break;
 	}
-	cout << "ur number is " << max;
+	cout << source1 << " " << op << " " << source2 << " = " << result;
 	return 0;
 }
 int square(int x) {
