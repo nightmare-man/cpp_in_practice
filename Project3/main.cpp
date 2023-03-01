8fc91908194f870884548d23cd0c95bedcc056aa
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 10:29:43 +0800

    prime2

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -2,20 +2,19 @@
 int square(int x);
 int main() {
 	vector<int> prime = { 2,3,5 };
-	constexpr int end = 1000;
+	constexpr int end = 1000000;
 	for (int i = 6; i <= end; i++) {
+		double max = sqrt(i);
 		bool isPrime = true;
-		for (int x : prime) {
-			if (i % x == 0) {
+		for (int j = 0; j < prime.size()&&prime[j]<=max; j++) {
+			if (i % prime[j] == 0) {
 				isPrime = false;
 				break;
 			}
 		}
 		if (isPrime) prime.push_back(i);
 	}
-	for (int x : prime) {
-		cout << x << endl;
-	}
+	for (int i : prime) cout << i << endl;
 	return 0;
 }
 int square(int x) {
