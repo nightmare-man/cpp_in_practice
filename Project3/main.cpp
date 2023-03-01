499bec4242203f5d017431dd5283965128976371
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 15:30:25 +0800

    chapter 5 error

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,29 +1,37 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	vector<string> words;
-	for (int o = 0;o<10; o++) {
-		string tmp="";
-		cin >> tmp;
-		words.push_back(tmp);
-	}
-	sort(words);
-	string mode = words[0];
-	int mode_time = 1;
-	int now_time = 1;
-	for (int i = 1; i < words.size(); i++) {
-		if (words[i] ==words[i - 1]) {
-			now_time++;
-		}
-		else {
-			if (now_time > mode_time) {
-				mode_time = now_time;
-				mode = words[i - 1];
+	vector<string> name;
+	vector<int> score;
+	string n = "";
+	int s = 0;
+	while (cin >> n >> s) {
+		bool existed = false;
+		for (string x: name) {
+			if (x == n) {
+				existed = true;
+				break;
 			}
-			now_time = 1;
+		}
+		if (!existed) {
+			name.push_back(n);
+			score.push_back(s);
 		}
 	}
-	cout << "mode " << mode << " times " << mode_time << endl;
+	for (int i = 0; i < name.size(); i++) {
+		cout << "name " << name[i] << " score " << score[i] << endl;
+	}
+	cout << "enter name ur want find:\n";
+	cin >> n;
+	bool existed = false;
+	for (int i = 0; i < name.size(); i++) {
+		if (name[i] == n) {
+			existed = true;
+			cout << "name " << name[i] << " score " << score[i] << endl;
+			break;
+		}
+	}
+	if (!existed) cout << "name not found\n";
 	return 0;
 }
 int square(int x) {
---
+++ Project4/main.cpp
@@ -0,0 +1,4 @@
+#include "../include/std_lib_facilities.h"
+int main() {
+
+}
