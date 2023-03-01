1f35b2563b84b57ed0c5db3534de67e060ffc011
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 10:55:38 +0800

    mode num

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,20 +1,29 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	vector<int> prime = { 2,3,5 };
-	constexpr int end = 1000000;
-	for (int i = 6; i <= end; i++) {
-		double max = sqrt(i);
-		bool isPrime = true;
-		for (int j = 0; j < prime.size()&&prime[j]<=max; j++) {
-			if (i % prime[j] == 0) {
-				isPrime = false;
-				break;
-			}
-		}
-		if (isPrime) prime.push_back(i);
+	vector<int> num;
+	for (int tmp; cin >> tmp;) {
+		num.push_back(tmp);
 	}
-	for (int i : prime) cout << i << endl;
+	sort(num);
+	int max = num[num.size() - 1];
+	int min = num[0];
+	int mode = num[0];
+	int mode_time = 1;
+	int now_time = 1;
+	for (int i = 1; i < num.size(); i++) {
+		if (num[i] == num[i - 1]) {
+			now_time++;
+		}
+		else {
+			if (now_time > mode_time) {
+				mode_time = now_time;
+				mode = num[i - 1];
+			}
+			now_time = 1;
+		}
+	}
+	cout << "mode " << mode << " times " << mode_time << endl;
 	return 0;
 }
 int square(int x) {
