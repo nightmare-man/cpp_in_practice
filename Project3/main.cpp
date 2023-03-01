6e3f8777573ddc2c9a539622b5612fff89a507d4
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 08:00:34 +0800

    mid value

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,36 +1,17 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	constexpr  double  cm_per_inch =2.54;
-	constexpr double cm_per_m = 100;
-	constexpr double inch_per_ft = 12;
 	vector<double> numbers;
-	double max=-32767;
-	double min=32767;
-	double tmp = 0;
-	string sign = "";
-	double sum = 0;
-	while (cin >> tmp>>sign) {
-		if (sign == "m") tmp *= cm_per_m;
-		else if (sign == "inch") tmp *= cm_per_inch;
-		else if (sign == "ft")  tmp *= (inch_per_ft * cm_per_inch);
-		else if (sign == "cm") tmp = tmp;
-		else {
-			cout << "undefined sign\n";
-			break;
-		}
-		cout << "now is " << tmp << "cm" << endl;
+	for (double tmp; cin >> tmp;) {
 		numbers.push_back(tmp);
-		sum += tmp;
-		if (tmp < min) min = tmp;
-		if (tmp > max)max = tmp;
-		cout << "the smallest so far:" << min << " the largest so far:" << max << endl;
 	}
-	cout << "the smallest:" << min << " the largest:" << max << " the sum:" << sum << " the number:" << numbers.size() << endl;
+	double mid = 0;
+	int size = numbers.size();
 	sort(numbers);
-	for (double s : numbers) {
-		cout << s << endl;
-	}
+
+	if (size % 2 == 0) mid = (numbers[size / 2 - 1] + numbers[size / 2]) / 2;
+	else mid = numbers[size / 2];
+	cout << "mid is " << mid;
 	return 0;
 }
 int square(int x) {
