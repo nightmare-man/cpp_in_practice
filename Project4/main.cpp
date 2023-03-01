67b4d4cc881f00def3c52a843bccfa81073c9eb3
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 15:42:24 +0800

    warning and error

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,4 +1,10 @@
 #include "../include/std_lib_facilities.h"
+int area(int lenght, int width);
 int main() {
-
+	char s1 = area(100,100);
+	cout << s1;
+	return 0;
+}
+int area(int lenght, int width) {
+	return lenght * width;
 }
