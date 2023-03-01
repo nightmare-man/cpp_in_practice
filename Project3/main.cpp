0b1c19d01e9b44ec8585b936c4bf992152d6db2a
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 10:59:17 +0800

    string mode

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,24 +1,24 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	vector<int> num;
-	for (int tmp; cin >> tmp;) {
-		num.push_back(tmp);
+	vector<string> words;
+	for (int o = 0;o<10; o++) {
+		string tmp="";
+		cin >> tmp;
+		words.push_back(tmp);
 	}
-	sort(num);
-	int max = num[num.size() - 1];
-	int min = num[0];
-	int mode = num[0];
+	sort(words);
+	string mode = words[0];
 	int mode_time = 1;
 	int now_time = 1;
-	for (int i = 1; i < num.size(); i++) {
-		if (num[i] == num[i - 1]) {
+	for (int i = 1; i < words.size(); i++) {
+		if (words[i] ==words[i - 1]) {
 			now_time++;
 		}
 		else {
 			if (now_time > mode_time) {
 				mode_time = now_time;
-				mode = num[i - 1];
+				mode = words[i - 1];
 			}
 			now_time = 1;
 		}
