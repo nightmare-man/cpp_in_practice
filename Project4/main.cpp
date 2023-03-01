9477a9b9655eb3527cb4d9d2f0d766add324246c
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 17:21:32 +0800

    前置条件与后置条件一起使用（结果检查）

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,6 +1,6 @@
 #include "../include/std_lib_facilities.h"
 /// <summary>
-///  参数要求a>0 b>a c>b
+///参数要求a>0 b>a c>b
 /// </summary>
 /// <param name="a"></param>
 /// <param name="b"></param>
@@ -10,9 +10,22 @@ int my_complicated_function(int a, int b, int c) {
 	if (!(a > 0 && a < b && b < c)) error("bad arguments for mcf");
 	return 0;
 }
+/// <summary>
+///length>0 width>0
+/// </summary>
+/// <param name="lengh"></param>
+/// <param name="width"></param>
+/// <returns></returns>
+int area(int lengh, int width) {
+	//if (lengh <= 0 || width <= 0) error("bad arguments");
+	int a = lengh * width;
+	if (a <= 0) error("area() pose-condition");
+	return a;
+}
 int main() {
 	try {
 		my_complicated_function(-1, 2, 3);
+		area(-1, -1);
 	}
 	catch (exception &e) {
 		cout << "exception :" << e.what() << endl;
