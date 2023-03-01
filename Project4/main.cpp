9bbb5c1b7aae495dc74a8654ab4eb6e7a30c13ce
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 18:43:01 +0800

    error

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,34 +1,20 @@
 #include "../include/std_lib_facilities.h"
-/// <summary>
-///参数要求a>0 b>a c>b
-/// </summary>
-/// <param name="a"></param>
-/// <param name="b"></param>
-/// <param name="c"></param>
-/// <returns></returns>
-int my_complicated_function(int a, int b, int c) {
-	if (!(a > 0 && a < b && b < c)) error("bad arguments for mcf");
-	return 0;
-}
-/// <summary>
-///length>0 width>0
-/// </summary>
-/// <param name="lengh"></param>
-/// <param name="width"></param>
-/// <returns></returns>
-int area(int lengh, int width) {
-	//if (lengh <= 0 || width <= 0) error("bad arguments");
-	int a = lengh * width;
-	if (a <= 0) error("area() pose-condition");
-	return a;
-}
+
 int main() {
 	try {
-		my_complicated_function(-1, 2, 3);
-		area(-1, -1);
+		int res = 7; vector<int> v(10); v[5] = res; cout << "success\n";
+		keep_window_open();
+		return 0;
 	}
-	catch (exception &e) {
-		cout << "exception :" << e.what() << endl;
+	catch (exception& e) {
+		cerr << "error:" << e.what() << '\n';
+		keep_window_open();
+		return 1;
+	}
+	catch (...) {
+		cerr << "oops:unknow exception!\n";
+		keep_window_open();
+		return 2;
 	}
 	return 0;
 }
