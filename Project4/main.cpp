f3f1765ff33bc9f624a70bcd146e6fcda909c887
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 19:26:29 +0800

    feb

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,22 +1,13 @@
 #include "../include/std_lib_facilities.h"
-void trySettle(double a, double b, double c, double& a1, double& a2) {
-	int flag = b * b - 4 * a * c;
-	if(flag< 0)error("bad arguments for trySettle()\n");
-	a1 = -b + sqrt(flag);
-	a2 = -b - sqrt(flag);
-	a1 /= 2 * a;
-	a2 /= 2 * a;
-}
+
 int main() {
-	double a, b, c;
-	double a1, a2;
-	try {
-		cin >> a >> b >> c;
-		trySettle(a, b, c, a1, a2);
+	int a = 1;
+	int b = 1;
+	while (a > 0 && b > 0) {
+		int c = b;
+		b = a + b;
+		a = c;
+		cout << a << endl;
 	}
-	catch (exception& e) {
-		cout << "exception :" << e.what() << endl;
-	}
-	cout << "answer:" << a1 << " " << a2 << endl;
 	return 0;
 }
