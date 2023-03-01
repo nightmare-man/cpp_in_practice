0746764855cdb6bf60c8fab8869586f14d68a2cc
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-01 09:57:57 +0800

    exponent

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,23 +1,15 @@
 #include "../include/std_lib_facilities.h"
 int square(int x);
 int main() {
-	vector<string> number = { "zero","one","two","three","four"\
-	,"five","six","seven","eight","nine" };
-	string in = "";
-	while (cin >> in) {
-		if (in.length() > 1) {
-			for (int i = 0; i<number.size(); i++) {
-				if (number[i] == in) {
-					cout << i<<endl;
-					break;
-				}
-			}
-		}
-		else {
-			char a = in[0];
-			int x = a - '0';
-			cout << number[x]<<endl;
-		}
+	constexpr int tar = 1000000000;
+	double cur_num = 1;
+	int spot = 1;
+	double sum=0;
+	while (spot <= 64) {
+		sum += cur_num;
+		cout << "spot " << spot << " cur " << cur_num << " sum " << sum << endl;
+		spot++;
+		cur_num *= 2;
 	}
 	return 0;
 }
