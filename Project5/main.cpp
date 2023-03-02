4e115ffd4154433580981afa8b191bec0637d489
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-02 08:38:39 +0800

    token caculator

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -1,34 +1,59 @@
 #include "../include/std_lib_facilities.h"
+class Token {
+public:
+	char kind;
+	double value;
+};
+Token get_token() {
+	char tmp = ' ';
+	Token t{'a',0};
+	string num="";
+	
+	while (cin >> tmp||num!="") {
+		bool end = false;
+		if (tmp >= '0' && tmp <= '9') {
+			num += tmp;
+		}
+		else {
+			if (num != "") {
+				int sum = 0;
+				for (int i = 0; i < num.size(); i++) {
+					sum += int(num[i] - '0') * pow(10, num.size() - 1);
+				}
+				t.value = sum;
+				cin.putback(tmp);
+				break;
+			}
+			
+			switch (tmp) {
+				case ' ':
+				case '\n':
+				case '\t':
+					break;
+				case '+':
+				case '-':
+				case '*':
+				case '/':
+					t.kind = tmp;
+					end = true;
+					break;
+			}
+		}
+		tmp = 0;
+		if (end) break;
+	}
+	return t;
+}
+vector<Token> tok;
 int main() {
 	cout << "Expression:";
-	int lval, rval;
-	char op;
-	cin >> lval;
-	int result;
-	bool end = false;
-	while (!end&&cin >> op) {
-		if (op == '=') break;
-		cin >> rval;
-		switch (op) {
-
-		case '+':
-			result = lval + rval;
-			break;
-		case '-':
-			result = lval - rval;
-			break;
-		case '*':
-			result = lval * rval;
-			break;
-		case '/':
-			result = lval / rval;
-			break;
-		default:
-			result = 0;
-			break;
-		}
-		lval = result;
+	Token t;
+	while (cin) {
+		t=get_token();
+		tok.push_back(t);
 	}
-	cout << "Result:" << result << endl;
+	for (Token t : tok) {
+		cout << "value " << t.value << " kind " << t.kind << endl;
+	}
 	return 0;
 }
