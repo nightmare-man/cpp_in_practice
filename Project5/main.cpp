f9b374b92f7120e7be9b63bcbbd5ac3a49b566cf
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-03 19:46:24 +0800

    number

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -1,60 +1,17 @@
 #include "../include/std_lib_facilities.h"
-string line="";
-int idx=0;
-vector<string> nv = { "birds","fish","C++" };
-vector<string> vv = { "rules","fly","swim" };
-vector<string> conjv = { "and","or","but" };
-bool noun() {
-	if (idx >= line.size()) return false;
-	bool ret = false;
-	for (string x : nv) {
-		if (line.find(x, idx) == idx) {
-			ret = true;
-			idx += x.size();
-			break;
-		}
+vector<string> ccin = { "thousand","hundred","ten","one" };
+int main() {
+	char ch;
+	string num = "";
+	int sum = 0;
+	for (int i = 0; i < 4; i++) {
+		cin >> ch;
+		if (!(ch >= '0' && ch <= '9')) break;
+		num += ch;
 	}
-	return ret;
-}
-bool verb() {
-	if (idx >= line.size()) return false;
-	bool ret = false;
-	for (string x : vv) {
-		if (line.find(x, idx) == idx) {
-			ret = true;
-			idx += x.size();
-			break;
-		}
+	cout << num << " is ";
+	for (int i = 0; i < num.size(); i++) {
+		cout << num[i] << " " << ccin[i + 4 - num.size()]<<" ";
 	}
-	return ret;
-}
-bool conj() {
-	if (idx >= line.size()) return false;
-	bool ret = false;
-	for (string x : conjv) {
-		if (line.find(x, idx) == idx) {
-			ret = true;
-			idx += x.size();
-			break;
-		}
-	}
-	return ret;
-}
-bool split() {
-	bool ret =idx<line.size()&&line[idx] == ' ';
-	while (idx < line.size()&&line[idx] == ' ') {
-		idx++;
-	}
-	return ret;
-}
-bool sentence() {
-	if (idx >= line.size()) return false;
-	bool ret = noun() && split() && verb();
-	if(idx<line.size()) ret=ret && split() && conj() && split() && sentence();
-	return ret;
-}
-int  main() {
-	line = "C++ fly and birds swim";
-	idx = 0;
-	cout << line << " is sentence?" << sentence() << endl;
+	return 0;
 }
