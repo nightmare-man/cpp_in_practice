d39168d69aa13ad203ec9db59ffe289f9411c899
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-03 15:41:30 +0800

    practice 2

--- material/calculator02bug.cpp
+++ material/calculator02bug.cpp
@@ -77,6 +77,7 @@ Token Token_stream::get()
     case '=':    // for "print"
     case 'x':    // for "quit"
     case '(': case ')': case '+': case '-': case '*': case '/':
+    case '{':case '}':
         return Token(ch);        // let each character represent itself
     case '.':
     case '0': case '1': case '2': case '3': case '4':
@@ -112,6 +113,12 @@ double primary()
         double d = expression();
         t = ts.get();
         if (t.kind != ')') error("')' expected");
+        return d;
+    }
+    case '{': {
+        double d = expression();
+        t = ts.get();
+        if (t.kind != '}') error(" '}' expected");
         return d;
     }
     case '8':            // we use '8' to represent a number
