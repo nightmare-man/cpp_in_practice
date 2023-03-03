a6ce6108b2c01ba68c5da49ac3a37beab3300be7
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-03 20:01:42 +0800

    排列组合

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -1,17 +1,43 @@
 #include "../include/std_lib_facilities.h"
-vector<string> ccin = { "thousand","hundred","ten","one" };
+int factoral(int x) {
+	if (x < 0) error("bad argument in factoral()\n");
+	if (x == 0) return 1;
+	int sum = 1;
+	for (int i = 1; i <= x; i++) sum *= i;
+	return sum;
+}
+
+/// <summary>
+/// a>=b>0
+/// </summary>
+/// <param name="a"></param>
+/// <param name="b"></param>
+/// <returns></returns>
+int Pab(int a,int b) {
+	if (a < b || b <= 0) error("bad argument in Pab()\n");
+	int sum = 1;
+	for (int i = 0; i < b; i++) {
+		sum *= (a - i);
+	}
+	return sum;
+}
 int main() {
-	char ch;
-	string num = "";
-	int sum = 0;
-	for (int i = 0; i < 4; i++) {
-		cin >> ch;
-		if (!(ch >= '0' && ch <= '9')) break;
-		num += ch;
+	int a = 0, b = 0;
+	char flag = 'x';
+	int result = 0;
+	cout << "enter two int a,b a>=b>0,p or c:\n";
+	cin >> a >> b >> flag;
+	switch (flag)
+	{
+	case 'p':
+		result = Pab(a, b);
+		break;
+	case 'c':
+		result = Pab(a, b) / factoral(b);
+		break;
+	default:
+		break;
 	}
-	cout << num << " is ";
-	for (int i = 0; i < num.size(); i++) {
-		cout << num[i] << " " << ccin[i + 4 - num.size()]<<" ";
-	}
+	cout << "result:" << result << endl;
 	return 0;
 }
