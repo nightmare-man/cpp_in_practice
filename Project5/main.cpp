e2a1a139ddf6768b7ed9f49bcbfb086555fccd1d
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-03 14:58:09 +0800

    Revert "caculator token_stream"
    This reverts commit 4d737f32ed9da9df1b7ee5875c6857a63488e770.

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -4,73 +4,76 @@ public:
 	char kind;
 	double value;
 };
-class Token_stream {
-public:
-	
-	Token get();
-	void putback(Token t);
-private:
-	bool full{ false };
-	Token buffer;
-};
+Token get_token() {
+	//todo 构建token_stream 类而不是函数
+	char tmp = 'x';
+	string num = "";
+	Token t{ 'n',0 };
+	bool end = false;
+	while (cin >> tmp) {
+		end = false;
+		if (tmp >= '0' && tmp <= '9') {
+			num += tmp;
+		}
+		else {
+			if (num != "") {
+				int len = num.length();
+				double n = 0;
+				for (int i = 0; i < len; i++) {
+					n += double(num[i] - '0') * pow(10, len - 1 - i);
+				}
+				t.value = n;
+				num = "";
+				cin.putback(tmp);
+				break;
+			}
+			else {
+				switch (tmp) {
+				case '+':
+				case '-':
+				case '*':
+				case '/':
+				case '(':
+				case ')':
+				case '=':
+				case '%':
+					t.kind = tmp;
+					end = true;
+					tmp = ' ';
+					break;
 
-void Token_stream::putback(Token t) {
-	if (full) error("putback() into a full buffer\n");
-	buffer = t;
-	full = true;
+				}
+			}
+		}
+		if (end) break;
+	}
+	return t;
 }
-Token Token_stream::get() {
-	if (full) {
-		full = false;
-		return buffer;
-	}
-	char ch;
-	cin >> ch;
-	switch (ch)
-	{
-	case '=':
-	case '(':case ')':case '+':case '-':case '*':case '/': {
-		Token t;
-		t.kind = ch;
-		t.value = 0;
-		return t;
-	}
-	case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':\
-	case '8':case '9': {
-		cin.putback(ch);
-		Token t;
-		t.kind = 'n';
-		cin >> t.value;
-		return t;
-	}
-	default: error("bad token\n"); break;
-	}
-}
-Token_stream ts;
+vector<Token> tok;
 double EE();
 double EE1();
 double EE2();
 
 double EE() {
 	double left = EE1();
-	Token t = ts.get();
+	Token t = get_token();
 	bool flag = true;
 	while (flag) {
 		//把所有高优先级的匹配到ee1，从左往右计算加减
 		switch (t.kind) {
 		case '+':
 			left += EE1();
-			t = ts.get();
+			t = get_token();
 			break;
 		case '-':
 			left -= EE1();
-			t = ts.get();
+			t = get_token();
 			break;
 		case '=':
 			flag = false;
 			break;
 		default:
-			ts.putback(t);
+			cin.putback(t.kind);
 			flag = false;
 			break;
 		}
@@ -79,30 +82,30 @@ double EE() {
 }
 double EE1() {
 	double left = EE2();
-	Token t = ts.get();
+	Token t = get_token();
 	bool flag = true;
 	while (flag) {
 		switch (t.kind) {
 		case '*':
 			left *= EE2();
-			t = ts.get();
+			t = get_token();
 			break;
 		case '/': {
 			double d = EE2();
 			if (d == 0) error("divide by zero!\n");
 			left /= d;
-			t =ts.get();
+			t = get_token();
 			break;
 		}
 		case '%': {
 			left = int(left) % int(EE2());
-			t = ts.get();
+			t = get_token();
 			break;
 		}
 		
 			
 		default:
-			ts.putback(t);
+			cin.putback(t.kind);
 			flag = false;
 			break;
 		}
@@ -111,36 +114,84 @@ double EE1() {
 }
 double EE2() {
 	double left = 0;
-	Token t = ts.get();
+	Token t = get_token();
 	if (t.kind == 'n') {
 		left = t.value;
 	}
 	else if (t.kind == '(') {
 		left = EE();
-		Token t=ts.get();
+		Token t=get_token();
 		if (t.kind != ')') error("expect )\n");
 	}
 	else {
-		ts.putback(t);
+		cin.putback(t.kind);
 	}
 	return left;
 }
-
-
-int main() {
-	try {
-		while (true) {
-			cout << "Expression:";
-			Token t;
-			double result = 0;
-			result = EE();
-			cout << "result " << result << endl;
+bool E(int left, int right, int& r);
+bool E1(int left, int right, int& r);
+bool E2(int left, int right, int& r);
+bool E2(int left, int right, int& r) {
+	if (left == right) return (tok[left].kind == 'n' && (r = tok[left].value));
+	if (left > right) return false;
+	if(right-left>1) return (tok[left].kind=='('&&E(left+1,right-1,r)&&tok[right].kind==')') ;
+	return false;
+}
+bool E1(int left, int right, int& r) {
+	int lval = 0;
+	int rval = 0;
+	bool flag = false;
+	for (int i = left; i <= right; i++) {
+		if (tok[i].kind == '*') {
+			if (E2(left, i - 1, lval) && E2(i + 1, right, rval)) {
+				flag = true;
+				r = lval * rval;
+				break;
+			}
+		}
+		if (tok[i].kind == '/') {
+			if (E2(left,i - 1, lval) && E2(i + 1, right, rval)) {
+				flag = true;
+				r = lval / rval;
+				break;
+			}
 		}
 	}
-	catch (runtime_error& e) {
-		cout << "runtime error:" << e.what() << endl;
+	return flag || (E2(left, right, r));
+}
+bool E(int left,int right,int&r) {
+	int lval = 0;
+	int rval = 0;
+	bool flag = false;
+	for (int i = left; i <= right; i++) {
+		if (tok[i].kind == '+') {
+			if (E1(left, i - 1, lval) && E1(i + 1, right, rval)) {
+				flag = true;
+				r = lval + rval;
+				break;
+			}
+		}
+		if (tok[i].kind == '-') {
+			if (E1(left, i - 1, lval) && E1(i + 1, right, rval)) {
+				flag = true;
+				r = lval - rval;
+				break;
+			}
+		}
 	}
-	
+	return flag || (E1(left, right, r));
+}
+
+int main() {
+	while (true) {
+		cin.clear();
+		cout << "Expression:";
+		Token t;
+		double result = 0;
+		result = EE();
+		
+		cout << "result " << result << endl;
+	}
 	
 	return 0;
 }
