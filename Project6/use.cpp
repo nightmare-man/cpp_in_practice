540de92b91cb71f4cef379de7d78af29a866e19a
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-04 18:36:54 +0800

    swap 引用 和常量引用

--- Project6/use.cpp
+++ Project6/use.cpp
@@ -1,7 +1,22 @@
 #include "my.h"
+void swap(int a, int b) {
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+void swap_r(int& a, int& b) {
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
 int main() {
-	foo = 7;
-	print_foo();
-	print(99);
+	const int x = 7;
+	const int y = 9;
+	swap_r(x, y);
+	print(x);
+	print(y);
 	return 0;
 }
