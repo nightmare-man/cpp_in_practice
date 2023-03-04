4f59d8c02ab92aaf4b636a6b883ed61a2e4a4b28
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-04 18:21:38 +0800

    chapter 8

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,9 @@
 #include "../include/std_lib_facilities.h"
 int main() {
-	cout << "hello" << endl;
-	keep_window_open();
+	char ch;
+	while (cin.get(ch)) {
+		cout << int(ch) << endl;
+	}
+	
 	return 0;
 }
