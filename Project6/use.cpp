2354e40bbe2bf49422b1b7b3fc8f7e1c367a0615
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-04 22:03:40 +0800

    自定义类型 如果定义了构造函数则不再提供默认构造函数，因此Date d;报错

--- Project6/use.cpp
+++ Project6/use.cpp
@@ -1,24 +1,18 @@
 #include "../include/std_lib_facilities.h"
-void fibonacci(int x,int y,vector<int>& v,int n) {
-	if (n < 2) error("bad argument\n");
-	int a = x;
-	int b = y;
-	int c =0 ;
+class Date {
+public:
+	Date(int y,int m,int d):year(y),month(m),day(d) {}
+	void print();
+private:
+	int year;
+	int month;
+	int day;
+};
+void Date::print() {
 
-	for (int i = 0; i < n-2; i++) {
-		v.push_back(a);
-		c = a + b;
-		a = b;
-		b = c;
-	}
-}
-void print(string tag,const vector<int>& v) {
-	cout << tag << ":";
-	for (int x : v)cout << x<< " ";
 }
 int main() {
-	vector<int> vint;
-	fibonacci(1, 1, vint, 100);
-	print("vector is", vint);
+	Date d;
+	d.print();
 	return 0;
 }
