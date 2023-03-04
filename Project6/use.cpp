20fb1a6436f1bbe7ddb7a6aea86dc9fcd18eb517
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-04 18:53:52 +0800

    引用传递参数

--- Project6/use.cpp
+++ Project6/use.cpp
@@ -1,35 +1,10 @@
 #include "../include/std_lib_facilities.h"
-namespace X {
-	int var = 7;
-	void print() {
-		cout << var << endl;
-	}
-}
-namespace Y {
-	int var = 9;
-	void print() {
-		cout << var << endl;
-	}
-}
-namespace Z {
-	int var = 11;
-	void print() {
-		cout << var << endl;
-	}
+void print(string tag, vector<int>& v) {
+	cout << tag << ":";
+	for (int x : v)cout << x<< " ";
 }
 int main() {
-	X::print();
-	X::var = 8;
-	using namespace Y;
-	var = 9;
-	print();
-	{
-		using Z::var;//这里是块作用域，单独使用using 部分 而不是using namespace
-		using Z::print;
-		var = 14;
-		print();
-	}
-	print();
-	X::print();
+	vector<int> vint{ 1,2,3,4,5,6,7 };
+	print("vector is", vint);
 	return 0;
 }
