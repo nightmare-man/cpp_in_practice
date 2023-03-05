f2e443c49ff44995756dddbcfc3a7448210ba1c0
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-05 10:04:12 +0800

    operator overloading & int to enum

--- Project6/use.cpp
+++ Project6/use.cpp
@@ -34,8 +34,14 @@ void Date::add_day(int n) {
 enum class Month {
 	jan=1,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec
 };
+enum Day {
+	Mon=1,Tue,Wed,Thu
+};
+Month operator++(Month& m) {
+	m = (m == Month::dec )?Month::jan:Month(int(m) + 1);
+	return m;
+}
 int main() {
-	Month m = jan;
-	Month n = Month::feb;
+	
 	return 0;
 }
