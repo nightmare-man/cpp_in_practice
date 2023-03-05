7211bab987428be85885ff0e46d6ca4982d46d7f
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-05 09:44:54 +0800

    inline memberfunction define

--- Project6/use.cpp
+++ Project6/use.cpp
@@ -1,18 +1,25 @@
 #include "../include/std_lib_facilities.h"
 class Date {
 public:
-	Date(int y,int m,int d):year(y),month(m),day(d) {}
+	Date(int y1,int m1,int d1):y(y1),m(m1),d(d1) {}
 	void print();
+	void add_day(int n);
+	int month() { return m; }
+	int year() { return y; }
+	int day() { return d; }
 private:
-	int year;
-	int month;
-	int day;
+	int y;
+	int m;
+	int d;
 };
 void Date::print() {
+	cout << y << " " << m << " "<<d;
+}
+void Date::add_day(int n) {
 
 }
 int main() {
-	Date d;
+	Date d(19, 10, 20);
 	d.print();
 	return 0;
 }
