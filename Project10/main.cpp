480840513b29ec64651f34a972c9b5d6bf678137
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-11 17:46:24 +0800

    link*链表

--- Project10/main.cpp
+++ Project10/main.cpp
@@ -1,59 +1,32 @@
 #include "../include/std_lib_facilities.h"
-class A {
-public:
-	A() { cout << "A construct\n"; }
-	~A() { cout << "A destruct\n"; }
+struct Link {
+	string value;
+	Link* prev;
+	Link* succ;
+	Link(string v,Link* p=nullptr,Link* s=nullptr):value(v),prev(p),succ(s) {}
 };
-class B :A {
-public:
-	B() { cout << "B construct\n"; }
-	~B() { cout << "B destruct\n"; }
-};
-class Myvector {
-public:
-	Myvector(int s) :size{ s }, mem{ new double[s] } {
-		//初始化
-		cout << "init\n";
-		for (int i = 0; i < s; i++) mem[i] = 0;
-	};
-	~Myvector() { delete[] mem;};
-	double get(int idx)const { return mem[idx]; };
-	void set(int idx, double val) { mem[idx] = val; };
-	int Size()const { return size; };
-
-private:
-	int size;
-	double* mem;
-};
-double* calc(int res, int max) {
-	//p用于局部使用，但是忘记释放
-	double* p = new double[max];
-	double* res1 = new double[res];
-	delete[] p;
-	//delete[]和new[]都会记录分配的数组大小
-	//但是指针却不记录，是为了保持指针的灵活性通用性
-	//因为指针不仅仅用来指向new分配的内存，也用来指向
-	//栈 或者静态内存空间等，这个时候不知道数组大小
-	return res1;
+void print_god(Link* r) {
+	while (r) {
+		cout << r->value << "->";
+		r = r->succ;
+	}
+}
+Link* insert(Link* p,Link* pre) {
+	//注意检测空指针
+	if (!p) return nullptr;
+	if (!pre) return p;
+	pre->succ = p;
+	if (p->prev) {
+		p->prev->succ = pre;
+		pre->prev = p->prev;
+	}
+	p->prev = pre;
+	return pre;
 }
 int main() {
-	//定义变量初始化为10
-	int y = 10;
-	//没有新变量，但是有符号r引用了y
-	int& r = y;
-	//通过r引用 y变为7
-	r = 7;
-	//定义变量y2 通过r 引用y给y2赋值7；
-	int y2 = r;
-	
-	int& r2 = y2;
-	//通过r2引用y2 r引用y 赋值
-	r2 = r;
-	//引用也是通过内存地址来实现的，与指针的区别
-	//是认为定义的特性
-	//指针常量 即指向不可变
-	int* const a = &y2;
-	//常量指针 指向不可变的量
-	int const* b = &y2;
-	return 0;
+	Link* gods1 = new Link{ "Thor" };
+	Link*gods2 = new Link{ "Odin"};
+	Link* gods3 = new Link{ "Freia" };
+	Link* head = insert(insert(gods1, gods2),gods3) ;
+	print_god(head);
 }
