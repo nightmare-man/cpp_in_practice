ff9a38dd92bf3e2f1c78198602c8266cbf38394b
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-11 23:07:10 +0800

    默认浅拷贝，只赋值元素，不复制指向的对象

--- Project11/main.cpp
+++ Project11/main.cpp
@@ -2,10 +2,19 @@
 class Myvector {
 public:
 	Myvector(initializer_list<double> lst) :size{ int(lst.size()) }, mem{ new double[size] } {
-		copy(lst.begin(), lst.end(), mem);
+		//容器的copy
+		std::copy(lst.begin(), lst.end(), mem);
 	};
-	double get(int idx)const{
+	double get(int idx)const {
+		if (idx < 0 || idx >= size) error("out of range\n");
 		return mem[idx];
+	};
+	void set(int idx,double val){
+		if (idx < 0 || idx >= size) error("out of range\n");
+		mem[idx] = val;
+	}
+	~Myvector() {
+		delete[] mem;
 	}
 private:
 	int size;
@@ -14,5 +23,8 @@ private:
 int main() {
 	//列表初始化容器initializer_list
 	Myvector v{ 1,2,3,4,5 };
-	cout << v.get(4);
+	Myvector v2{ 0,0,0,0,0 };
+	//析构函数会报错，原因是浅拷贝，导致v原来分配的内存
+	//被释放两次 而v2原来的的没释放
+	cout << v2.get(4);
 }
