1f131371086be31569cc173c97be0bea286debdd
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-11 21:39:59 +0800

    this指针 成员函数类使用

--- Project10/main.cpp
+++ Project10/main.cpp
@@ -1,18 +1,20 @@
 #include "../include/std_lib_facilities.h"
-struct Link {
-	string value;
+class Link {
+public:
+	Link(string v, Link* p = nullptr, Link* s = nullptr) :value{ v }, prev{ p }, succ{s} {}
+	Link* insert(Link* pre);
+	Link* add(Link* suc);
+	Link* find(const string value) const;
+	Link* erase();
+	Link* advance(int step)const;
+	void print_all()const;
 	Link* prev;
 	Link* succ;
-	Link(string v,Link* p=nullptr,Link* s=nullptr):value(v),prev(p),succ(s) {}
+	string value;
 };
-void print_god(Link* r) {
-	while (r) {
-		cout << r->value << "->";
-		r = r->succ;
-	}
-}
-Link* insert(Link* p,Link* pre) {
-	//注意检测空指针
+Link* Link::insert(Link* pre) {
+	//this 指针；
+	Link* p = this;
 	if (!p) return nullptr;
 	if (!pre) return p;
 	pre->succ = p;
@@ -23,7 +25,8 @@ Link* insert(Link* p,Link* pre) {
 	p->prev = pre;
 	return pre;
 }
-Link* add(Link* p, Link* suc) {
+Link* Link::add(Link* suc) {
+	Link* p = this;
 	if (!p) return nullptr;
 	if (!suc) return p;
 	suc->prev = p;
@@ -34,63 +37,60 @@ Link* add(Link* p, Link* suc) {
 	p->succ = suc;
 	return p;
 }
-//删除节点 返回p的前驱或者后继 优先返回后继，除非只有前驱
-Link* erase(Link*p) {
-	if (!p) return nullptr;
-	Link* ret = nullptr;
-	if (p->prev) {
-		ret = p->prev;
-		p->prev->succ = p->succ;
-	}
-	if (p->succ) {
-		ret = p->succ;
-		p->succ->prev = p->prev;
-	}
-	return ret;
-}
-/// <summary>
-/// 在p及后继中找val
-/// </summary>
-/// <param name="p"></param>
-/// <param name="val"></param>
-/// <returns></returns>
-Link* find(Link* p,string val) {
-	while (p&&p->value!=val) {
+Link* Link::find(const string v) const{
+	Link* p =const_cast<Link*>( this);
+	while (p && p->value != v) {
 		p = p->succ;
 	}
 	return p;
 }
 /// <summary>
-/// step向后移动x个节点
+/// 从列表中移除本节点 优先返回suc 
 /// </summary>
-/// <param name="p"></param>
-/// <param name="step"></param>
 /// <returns></returns>
-Link* advance(Link* p, int step) {
-	if (!p) return nullptr;
-	if (step== 0) {
-		return p;
+Link* Link::erase() {
+	Link* p = this;
+	Link* ret = nullptr;
+	if (p->prev) {
+		p->prev->succ = p->succ;
+		ret =p->prev;
 	}
-	else if(step>0){
-		while (p && step != 0) {
-			p = p->succ;
-			step--;
-		}
-		return p;
+	if (p->succ) {
+		p->succ->prev = p->prev;
+		ret = p->succ;
 	}
-	else {
-		while (p && step != 0) {
-			p = p->prev;
-			step++;
-		}
-		return p;
+	return ret;
+}
+void Link::print_all()const {
+	const Link* p = this;
+	cout << "{";
+	while (p) {
+		cout << p->value<<',';
+		p = p->succ;
 	}
+	cout << "}\n";
+	return;
 }
 int main() {
-	Link* gods1 = new Link{ "Thor" };
-	Link*gods2 = new Link{ "Odin"};
-	Link* gods3 = new Link{ "Freia" };
-	Link* head = insert(insert(gods1, gods2),gods3) ;
+	Link* norse_god = new Link{ "Thor" };
+	norse_god = norse_god->insert(new Link{ "Odin" });
+	norse_god = norse_god->insert(new Link{ "Zeus" });
+	norse_god = norse_god->insert(new Link{ "Freia" });
 
-	print_god(erase(gods2));
+	Link* greek_god = new Link{ "Hera" };
+	greek_god = greek_god->insert(new Link{ "Athena" });
+	greek_god = greek_god->insert(new Link{ "Mars" });
+	greek_god = greek_god->insert(new Link{ "Poseidon" });
+	Link* p = greek_god->find("Mars");
+	if (p) p->value = "Ares";
+	greek_god->print_all();
+	Link* p2 = norse_god->find("Zeus");
+	if (p2) {
+		if (p2 == norse_god)norse_god = norse_god->succ;
+		p2->erase();
+		greek_god = greek_god->insert(p2);
+	}
+	norse_god->print_all();
+	greek_god->print_all();
+	return 0;
 }
