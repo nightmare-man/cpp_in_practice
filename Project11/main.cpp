5fc665aad23c3de16d10013b65815eb028a6f08a
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-12 14:05:42 +0800

    拷贝复制

--- Project11/main.cpp
+++ Project11/main.cpp
@@ -5,6 +5,9 @@ public:
 		//容器的copy
 		std::copy(lst.begin(), lst.end(), mem);
 	};
+	Myvector(int s) :size{ s }, mem{new double[s]} {
+		for (int i = 0; i < s; i++) mem[i] = 0;
+	}
 	int Size()const { return size; };
 	double get(int idx)const {
 		if (idx < 0 || idx >= size) error("out of range\n");
@@ -19,6 +22,8 @@ public:
 	Myvector(const Myvector& arg) :size{ arg.size }, mem{new double[size]} {
 		std::copy(arg.mem, arg.mem+size, mem);
 	}
+	//拷贝赋值
+	Myvector& operator=(const Myvector& v);
 	~Myvector() {
 		delete[] mem;
 	}
@@ -26,9 +31,20 @@ private:
 	int size;
 	double* mem;
 };
+Myvector& Myvector::operator=(const Myvector& v) {
+	double* p = new double[v.size];
+	std::copy(v.mem, v.mem + v.size, p);
+	delete[] mem;
+	mem = p;
+	size = v.size;
+	return *this;
+}
 int main() {
-	Myvector v1{ 1,2,3,4,5 };
-	Myvector v2 = v1;
-	cout << v2.get(3);
+	Myvector v(3);
+
+	Myvector v1(4);
+
+	v1 = v;
+
 	return 0;
 }
