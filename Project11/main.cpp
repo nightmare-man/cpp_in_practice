eec9dcd27e8686626d2eadf36158cdaaa6cc9bd0
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-12 14:39:31 +0800

    移动构造函数 移动赋值函数

--- Project11/main.cpp
+++ Project11/main.cpp
@@ -4,9 +4,11 @@ public:
 	Myvector(initializer_list<double> lst) :size{ int(lst.size()) }, mem{ new double[size] } {
 		//容器的copy
 		std::copy(lst.begin(), lst.end(), mem);
+		cout << "调用了列表初始化\n";
 	};
 	Myvector(int s) :size{ s }, mem{new double[s]} {
 		for (int i = 0; i < s; i++) mem[i] = 0;
+		cout << "调用了初始化\n";
 	}
 	int Size()const { return size; };
 	double get(int idx)const {
@@ -17,12 +19,20 @@ public:
 		if (idx < 0 || idx >= size) error("out of range\n");
 		mem[idx] = val;
 	}
-	//拷贝构造函数
-	//隐式类型转换 Myvector&arg->const Myvector&
+	
 	Myvector(const Myvector& arg) :size{ arg.size }, mem{new double[size]} {
 		std::copy(arg.mem, arg.mem+size, mem);
+		cout << "调用了拷贝初始化\n";
 	}
-	//拷贝赋值
+
+	Myvector(Myvector&& v)noexcept :size{ v.size }, mem{v.mem} {
+		v.size = 0;
+		v.mem = nullptr;
+		cout << "调用了移动初始化\n";
+	}
+	
+	Myvector& operator=(Myvector&& v)noexcept;
+	
 	Myvector& operator=(const Myvector& v);
 	~Myvector() {
 		delete[] mem;
@@ -31,19 +41,24 @@ private:
 	int size;
 	double* mem;
 };
+Myvector& Myvector::operator=(Myvector&& v) noexcept{
+	size = v.size;
+	mem = v.mem;
+	v.size = 0;
+	v.mem = nullptr;
+	cout << "调用了移动赋值\n";
+	return *this;
+}
 Myvector& Myvector::operator=(const Myvector& v) {
 	double* p = new double[v.size];
 	std::copy(v.mem, v.mem + v.size, p);
 	delete[] mem;
 	mem = p;
 	size = v.size;
+	cout << "调用了拷贝赋值\n";
 	return *this;
 }
-/// <summary>
-/// 函数返回对象，并用来赋值（拷贝赋值），该对象成为零时对象，复制
-/// 会被销毁
-/// </summary>
-/// <returns></returns>
+
 Myvector f1() {
 	Myvector res{ 1,2,3 };
 	return res;
@@ -52,8 +67,8 @@ int main() {
 	Myvector v(3);
 
 	Myvector v1(4);
-
+	//编译器进行了优化，没有调用移动构造
+	//而是直接把返回的右值对象改成左值成为了v2
 	Myvector v2 = f1();
-	cout << v2.get(1);
 	return 0;
 }
