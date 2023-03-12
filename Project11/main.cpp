495080980d4732f00f1d6b97c34342af97eb2f8a
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-12 22:28:48 +0800

    cin字符数组输入 补0

--- Project11/main.cpp
+++ Project11/main.cpp
@@ -1,78 +1,40 @@
 #include "../include/std_lib_facilities.h"
-class Myvector {
-public:
-	Myvector(initializer_list<double> lst) :size{ int(lst.size()) }, mem{ new double[size] } {
-		//容器的copy
-		std::copy(lst.begin(), lst.end(), mem);
-		cout << "调用了列表初始化\n";
-	};
-	Myvector(int s) :size{ s }, mem{new double[s]} {
-		for (int i = 0; i < s; i++) mem[i] = 0;
-		cout << "调用了初始化\n";
+bool is_pal(const string& s) {
+	int first = 0;
+	int last = s.length() - 1;
+	while (first < last) {
+		if (s[first] != s[last]) return false;
+		first++;
+		last--;
 	}
-	int Size()const { return size; };
-	double& operator[](int n) { 
-		if (n < 0 || n >= size) error("out of range");
-		return mem[n]; 
-	}
-	//适用于const的Myvector对象使用[]
-	double operator[](int n) const {
-		if (n < 0 || n >= size) error("out of range");
-		return mem[n];
-	}
-	Myvector(const Myvector& arg) :size{ arg.size }, mem{new double[size]} {
-		std::copy(arg.mem, arg.mem+size, mem);
-		cout << "调用了拷贝初始化\n";
-	}
-
-	Myvector(Myvector&& v)noexcept :size{ v.size }, mem{v.mem} {
-		v.size = 0;
-		v.mem = nullptr;
-		cout << "调用了移动初始化\n";
-	}
-	
-	Myvector& operator=(Myvector&& v)noexcept;
-	
-	Myvector& operator=(const Myvector& v);
-	~Myvector() {
-		delete[] mem;
-	}
-private:
-	int size;
-	double* mem;
-};
-Myvector& Myvector::operator=(Myvector&& v) noexcept{
-	size = v.size;
-	mem = v.mem;
-	v.size = 0;
-	v.mem = nullptr;
-	cout << "调用了移动赋值\n";
-	return *this;
+	return true;
 }
-Myvector& Myvector::operator=(const Myvector& v) {
-	double* p = new double[v.size];
-	std::copy(v.mem, v.mem + v.size, p);
-	delete[] mem;
-	mem = p;
-	size = v.size;
-	cout << "调用了拷贝赋值\n";
-	return *this;
+bool is_pal1(const char s[],int size) {
+	int first = 0;
+	int last = size - 1;
+	while (first < last) {
+		if (s[first] != s[last]) return false;
+		first++;
+		last--;
+	}
+	return true;
 }
-
-Myvector f1() {
-	Myvector res{ 1,2,3 };
-	return res;
+istream& read_word(istream& is, char* buff, int max) {
+	is.width(max);//最多读max-1;
+	is >> buff;
+	return is;
 }
-void f(const Myvector& v) {
-	//调用了const版本的[]
-	double d = v[1];
-	cout << d;
+int strlen(char s[]) {
+	int cnt = 0;
+	while (*s) {
+		s++;
+		cnt++;
+	}
+	return cnt;
 }
 int main() {
-	//数组初始化
-	int ai[] = { 1,2,3,4,5 };
-	int ai1[100] = { 1,2,3,4 };//后面的都初始化为0；
-	double ai2[100] = {};//全部初始为0
-	const char a[] = "woshinihao";
-	return 0;
+	constexpr int max = 128;
+	char s[max];
+	//cin输入字符数组时会自动补0
+	cin >> s;
 }
