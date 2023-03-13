cf4de32c58ea8410aec63c51e576ac3818a9561c
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-13 16:31:40 +0800

    构造函数 析构函数 与try catch 的汇编实现

--- Project11/main.cpp
+++ Project11/main.cpp
@@ -1,11 +1,20 @@
-#include "../include/std_lib_facilities.h"
-void suspicious(int s, int x) {
-	int* p = new int[s];
-	//异常导致delete无法执行内存泄露
-	error("nihao");
-	delete[] p;
-	return;
-}
+class Aaaaaa {
+public:
+	int a;
+	Aaaaaa() { a++; };
+	~Aaaaaa() { a--; };
+};
 int main() {
-	suspicious(100,200);
+	//定义一个变量，实际就是分类内存并在函数开始的时候调用构造函数
+	//结尾的时候也会调用析构函数
+	//异常处理的catch块是一个单独的函数，里面也会调用析构函数
+	Aaaaaa a;
+	try {
+		a.a = 10;
+		throw;
+	}
+	catch (...) {
+		a.a =a.a + 20;
+	}
+	return 0;
 }
