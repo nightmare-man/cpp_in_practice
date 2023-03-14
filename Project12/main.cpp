2d130a74da401ab960398f11f9ea48562b569839
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-14 17:04:37 +0800

    用模板和迭代器实现链表

--- Project12/main.cpp
+++ Project12/main.cpp
@@ -1,30 +1,17 @@
 #include "../include/std_lib_facilities.h"
 
-int* get_from_j(int* count) {
-	return new int[3] {1, 2, 3};
-}
-vector<int>* get_from_i() {
-	return new vector<int>{ 1,2,3 };
-}
-template<typename Iterator>
-Iterator high(Iterator first, Iterator last) {
-	Iterator high = first;
-	for (Iterator p = first; p != last; ++p) {
-		if (*high < *p) high = p;
-	}
-	return high;
-}
+template<typename Elem>
+struct Link {
+	Link* prev;
+	Link* succ;
+	Elem val;
+};
+//链表的定义
+template<typename Elem>
+struct List {
+	Link<Elem>* first;
+	Link<Elem>* last;
+};
 int main() {
-	int jc = 0;
-	int* jd = get_from_j(&jc);
-	int* jh = high(jd, jd + jc);
 
-	vector<int>* id = get_from_i();
-	vector<int>& v = *id;
-	int* ih = high(&v[0], &v[0] + v.size());
-	delete[] jd;
-	int* p = &(*id)[0];
-	delete id;
-	cout<<*p;
-	return 0;
 }
