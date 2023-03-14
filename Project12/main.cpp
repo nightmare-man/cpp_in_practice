861ad72d7a8af6d84359a7eeec308634f7475fb4
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-14 23:14:36 +0800

    性能upup

--- Project12/main.cpp
+++ Project12/main.cpp
@@ -8,14 +8,15 @@ private:
 	list<Line>::iterator lpos;
 	Line::iterator cpos;
 public:
-	//这里有个坑，前一个参数是传递引用
-	//后一个参数不是，为啥呢，因为
+	//这里有个坑，为啥不是传递引用而是传值呢？
 	//vector<char> 的begin()返回是一个右值
 	//临时的，如果想直接用的话这里就不能用引用
 	//如果想用引用 得
 	//vector<char>::iterator p= vector<char>.bein()
 	//再传p作为第二个参数
-	Text_iterator(list<Line>::iterator& ll, Line::iterator& cc) :lpos{ ll }, cpos{ cc } {};
+	// 而采用这种方法，会多定义一个中间局部变量，浪费性能
+	//直接调用移动赋值快
+	Text_iterator(list<Line>::iterator ll, Line::iterator cc) :lpos{ ll }, cpos{ cc } {};
 	bool operator==(const Text_iterator& b) { return (lpos == b.lpos) && (cpos == b.cpos); };
 	bool operator!=(const Text_iterator& b) { return !((*this) == b); }
 	char& operator*() {
@@ -41,9 +42,7 @@ public:
 struct Doc {
 	list<Line> line;
 	Text_iterator begin() {
-		auto p = line.begin();
-		auto p1 = (*p).begin();
-		return Text_iterator(p, p1);
+		return Text_iterator(line.begin(),(*line.begin()).begin() );
 	}
 
 
