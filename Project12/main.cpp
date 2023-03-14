b903c1544f3e6f422b1e612c7a767badc32ec393
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-14 10:43:43 +0800

    引用与指针常量

--- Project12/main.cpp
+++ Project12/main.cpp
@@ -8,4 +8,13 @@ int main() {
     //移动构造函数
     vector<double> v = f();
     cout << v[0];
+    int* const p = new int;
+    //用引用指向指针指的位置，这样就不用再
+    //频繁操作指针，防止指针修改过程中
+    //的错误 相当于 int* const p;
+    int a = 10;
+    p = &a;
+    int& z = *p;
+    cout << z;
+    return 0;
 }
