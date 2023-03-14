31fc45f66968813449601d3a42399c03f0932fb8
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-14 17:02:14 +0800

    iterator

--- Project12/main.cpp
+++ Project12/main.cpp
@@ -1,27 +1,30 @@
 #include "../include/std_lib_facilities.h"
-void copy(int* f1, int* e1, int* f2) {
-	if (f1 == e1) return;
-	for (; f1 != e1; ) {
-		*f2 = *f1;
-		f1++;
-		f2++;
-	}
+
+int* get_from_j(int* count) {
+	return new int[3] {1, 2, 3};
 }
-//迭代器 指向序列中的某个元素，可以做一系列操作
-//可以是指针 也可以是引用等
-//泛型比较
-template<typename Iterator> //要求传入的类型为容器类型
-Iterator high(Iterator s,Iterator e) {
-	Iterator h = s;
-	for (; s != e; s++) {
-		if (*s > *h)h = s;
+vector<int>* get_from_i() {
+	return new vector<int>{ 1,2,3 };
+}
+template<typename Iterator>
+Iterator high(Iterator first, Iterator last) {
+	Iterator high = first;
+	for (Iterator p = first; p != last; ++p) {
+		if (*high < *p) high = p;
 	}
-	return h;
+	return high;
 }
 int main() {
-	//迭代器 iterator类
-	vector<int> p{ 1,2,3,44,4,5 };
-	vector<int>::iterator a = high(p.begin(), p.end());
-	cout << *a;
+	int jc = 0;
+	int* jd = get_from_j(&jc);
+	int* jh = high(jd, jd + jc);
+
+	vector<int>* id = get_from_i();
+	vector<int>& v = *id;
+	int* ih = high(&v[0], &v[0] + v.size());
+	delete[] jd;
+	int* p = &(*id)[0];
+	delete id;
+	cout<<*p;
 	return 0;
 }
