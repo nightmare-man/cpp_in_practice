8742811ef35264d43d597f586ef2d9d3da00619b
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-14 21:46:19 +0800

    notepad

--- Project12/main.cpp
+++ Project12/main.cpp
@@ -1,51 +1,28 @@
 #include "../include/std_lib_facilities.h"
-//类定义的内部，Link<Elem>简写为Link
-//如果是外部定义成员函数 则需要Link<Elem>
-template<typename Elem>
-struct Link {
-	Link* prev;
-	Link* succ;
-	Elem val;
+using Line = vector<char>;
+struct Doc {
+	list<Line> line;
+	//构造函数添加空行
+	Doc() { line.push_back(Line{}); }
 };
-template<typename Elem>
-class List {
-public:
-	class iterator;
-	iterator begin();
-	iterator end();
-	iterator insert(iterator p, const Elem& v);
-	iterator erase(iterator p);
-	void push_back(const Elem& v);
-	void push_front(const Elem& v);
-	void pop_front();
-	void pop_back();
-	Elem& front();
-	Elem& last();
-};
-template<typename Elem>
-class List<Elem>::iterator {
-private:
-	Link<Elem>* cur;
-public:
-	iterator(Link<Elem>* p) :cur(p) {};
-	iterator& operator--() { cur = cur->prev; return *this; }
-	iterator& operator++() { cur = cur->succ; return *this; }
-	Elem& operator*() { return cur->val; }
-	bool operator==(const iterator& b) { return cur == b.cur; }
-	bool operator!=(const iterator& b) { return !(*this == b); }
-};
-template<typename Iter>
-Iter high(Iter first, Iter last) {
-	Iter high = first;
-	for (Iter p = first; p != last; p++) {
-		if (*high < *p) high = p;
+istream& operator>>(istream& is, Doc& d) {
+	//这里使用get而不是cin>> 后者会吞掉\n
+	for (char ch; is.get(ch);) {
+		d.line.back().push_back(ch);
+		if (ch == '\n') d.line.push_back(Line{});
 	}
-	return high;
+	if (d.line.back().size()) d.line.push_back(Line{});
+	return is;
+}
+ostream& operator<<(ostream& os, Doc d) {
+	for (const auto p : d.line) {
+		for (const auto x : p) cout << x;
+	}
+	return os;
 }
 int main() {
-	//未定义的错误
-	int* a;
-	cout << &a[0];
+	Doc d;
+	cin >> d;
+	cout << d;
 	return 0;
 }
-
