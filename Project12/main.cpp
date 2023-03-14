2316c9a2ec26ed28e687051d78f4632404453a52
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-14 23:10:16 +0800

    左值右值

--- Project12/main.cpp
+++ Project12/main.cpp
@@ -1,12 +1,58 @@
-#include "../include/std_lib_facilities.h"
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
 using Line = vector<char>;
+class Text_iterator {
+private:
+	list<Line>::iterator lpos;
+	Line::iterator cpos;
+public:
+	//这里有个坑，前一个参数是传递引用
+	//后一个参数不是，为啥呢，因为
+	//vector<char> 的begin()返回是一个右值
+	//临时的，如果想直接用的话这里就不能用引用
+	//如果想用引用 得
+	//vector<char>::iterator p= vector<char>.bein()
+	//再传p作为第二个参数
+	Text_iterator(list<Line>::iterator& ll, Line::iterator& cc) :lpos{ ll }, cpos{ cc } {};
+	bool operator==(const Text_iterator& b) { return (lpos == b.lpos) && (cpos == b.cpos); };
+	bool operator!=(const Text_iterator& b) { return !((*this) == b); }
+	char& operator*() {
+		return *cpos;
+	}
+	Text_iterator& operator++() {
+		if (cpos == (*lpos).end()) {
+			lpos++;
+			cpos = (*lpos).begin();
+		}
+		else cpos++;
+		return *this;
+	}
+	Text_iterator& operator--() {
+		if (cpos == (*lpos).begin()) {
+			lpos--;
+			cpos = (*lpos).end();
+		}
+		else cpos--;
+		return *this;
+	}
+};
 struct Doc {
 	list<Line> line;
+	Text_iterator begin() {
+		auto p = line.begin();
+		auto p1 = (*p).begin();
+		return Text_iterator(p, p1);
+	}
+
+
 	//构造函数添加空行
 	Doc() { line.push_back(Line{}); }
 };
+
 istream& operator>>(istream& is, Doc& d) {
-	//这里使用get而不是cin>> 后者会吞掉\n
+
 	for (char ch; is.get(ch);) {
 		d.line.back().push_back(ch);
 		if (ch == '\n') d.line.push_back(Line{});
@@ -14,15 +60,10 @@ istream& operator>>(istream& is, Doc& d) {
 	if (d.line.back().size()) d.line.push_back(Line{});
 	return is;
 }
-ostream& operator<<(ostream& os, Doc d) {
-	for (const auto p : d.line) {
-		for (const auto x : p) cout << x;
-	}
-	return os;
-}
+
 int main() {
 	Doc d;
 	cin >> d;
-	cout << d;
+
 	return 0;
 }
