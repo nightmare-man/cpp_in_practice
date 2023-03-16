bc6e4ef0a400ac2e184d5f0f535b9e279d5cfd63
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-16 21:04:13 +0800

    输入输出流迭代器

--- Project13/main.cpp
+++ Project13/main.cpp
@@ -1,9 +1,23 @@
-#include <unordered_map>
-#include <string>
-#include <iostream>
-using namespace std;
+#include "../include/std_lib_facilities.h"
+
 int main() {
-	unordered_map< string,int> words;
-	for (string s; cin >> s;) ++words[s];
-	for (const auto& p : words) cout << p.first << " " << p.second << endl;
+	string from, to;
+	cin >> from >> to;
+	ifstream is{ from };
+	ofstream os{ to };
+	istream_iterator<string> ii{ is };
+	//当输入流结束时，遇到eof时，其iterator等于默认iterator，因此
+	//可以用eos{}当序列末尾
+	istream_iterator<string> eos;
+	//以"\n"作为分隔符
+	ostream_iterator<string> oo{ os,"\n"};
+	//使用指向string的迭代器初始化vector<string>
+
+	vector<string> b{ ii,eos };
+	//对vector进行排序
+	//sort(b.begin(), b.end());
+	//复制到输出流
+	//删除相邻的复制，因此最好先排序
+	unique_copy(b.begin(), b.end(), oo);
+	return 0;
 }
