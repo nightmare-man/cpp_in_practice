f7d8db293a2d7c5d15506affe6514744973e74c4
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-16 22:18:05 +0800

    practice

--- Project13/main.cpp
+++ Project13/main.cpp
@@ -1,18 +1,36 @@
 #include "../include/std_lib_facilities.h"
-template<typename in,typename out,typename Fun>
-out copy_if1(in start, in end, out res, Fun f) {
-	while (start != end) {
-		if (f(*start)) {
-			*res = *start;
-			res++;
-		} 
-		start++;
-	}
-	return res;
+struct Item {
+	string name;
+	int iid;
+	double value;
+};
+istream& operator>>(istream& in,Item& v) {
+	in >> v.name >> v.iid >> v.value;
+	//if (!in) error("bad input");
+	return in;
+}
+ostream& operator<<(ostream& os, const Item& v) {
+	os << "{" << v.name << "," << v.iid << "," << v.value << "}\n";
+	return os;
 }
 int main() {
-	int a[]{ 1,2,3,4,5 };
-	vector<int> p(5);
-	copy_if1(a, a + 5, p.begin(), [](int a) {return a % 2; });
-	for (auto a : p) cout << a << endl;
+	string from;
+	cin >> from;
+	if (!cin)error( "no input" );
+	ifstream is{ from };
+	vector<Item> x;
+	if (!is) error("open file error");
+	while (true) {
+		Item p;
+		is >> p;
+		if (!is) break;
+		x.push_back(p);
+	}
+	sort(x.begin(), x.end(), [](const Item& a, const Item& b) {
+		return a.iid < b.iid;
+		});
+	for (auto p : x) {
+		cout << p;
+	}
+	return 0;
 }
