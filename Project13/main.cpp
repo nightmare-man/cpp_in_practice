09636f3864f9f74686ecec6ef31cc5a28165abaa
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-16 21:46:39 +0800

    copy_if

--- Project13/main.cpp
+++ Project13/main.cpp
@@ -1,23 +1,18 @@
 #include "../include/std_lib_facilities.h"
-
+template<typename in,typename out,typename Fun>
+out copy_if1(in start, in end, out res, Fun f) {
+	while (start != end) {
+		if (f(*start)) {
+			*res = *start;
+			res++;
+		} 
+		start++;
+	}
+	return res;
+}
 int main() {
-	string from, to;
-	cin >> from >> to;
-	ifstream is{ from };
-	ofstream os{ to };
-	istream_iterator<string> ii{ is };
-	//当输入流结束时，遇到eof时，其iterator等于默认iterator，因此
-	//可以用eos{}当序列末尾
-	istream_iterator<string> eos;
-	//以"\n"作为分隔符
-	ostream_iterator<string> oo{ os,"\n"};
-	//使用指向string的迭代器初始化vector<string>
-
-	vector<string> b{ ii,eos };
-	//对vector进行排序
-	//sort(b.begin(), b.end());
-	//复制到输出流
-	//删除相邻的复制，因此最好先排序
-	unique_copy(b.begin(), b.end(), oo);
-	return 0;
+	int a[]{ 1,2,3,4,5 };
+	vector<int> p(5);
+	copy_if1(a, a + 5, p.begin(), [](int a) {return a % 2; });
+	for (auto a : p) cout << a << endl;
 }
