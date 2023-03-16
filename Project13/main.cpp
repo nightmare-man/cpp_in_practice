34dbdeb178ef6110eadde33f82daed53d8610626
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-16 15:31:36 +0800

    lambda

--- Project13/main.cpp
+++ Project13/main.cpp
@@ -13,9 +13,10 @@ it find_if1(it start, it end, Fun f) {
 bool odd(int x) {
 	return x % 2;
 }
-//函数对象，用成员作为内部参数
+//函数对象，用成员作为内部参数--内部变量又称状态
 //重载()运算符
 //比起全局变量做函数内部参数更容易维护
+//且方便生产一个函数的不同内部参数版本
 class Large_than {
 private:
 	int val;
@@ -25,8 +26,15 @@ public:
 };
 int main() {
 	vector<int> p{ 2,4,6 };
-	auto it = find_if1(p.begin(), p.end(), Large_than(6));
+	//auto it = find_if1(p.begin(), p.end(), Large_than{6});
+	//使用lambda表达式，代替函数对象
+	//注意，除了stl算法部分，尽量
+	//不使用函数对象和lambda
+	//使用 c class stl就ok了
+	auto it = find_if1(p.begin(), p.end(), [](int x) {return x > 5;
+		});
 	if (it == p.end()) cout << "not find";
 	else cout << "find";
+	cout << Large_than{ 6 }(10);
 	return 0;
 }
