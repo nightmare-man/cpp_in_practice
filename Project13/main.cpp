791d7496e95ad86b9a613a91adbbfffd1593187d
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-16 17:12:10 +0800

    unordered_map

--- Project13/main.cpp
+++ Project13/main.cpp
@@ -1,25 +1,9 @@
-#include "../include/std_lib_facilities.h"
-template<typename it,typename T>
-T accumulate1(it start,it end,T init){
-	while (start != end) {
-		init += *start;
-		start++;
-	} 
-	return init;
-}
-//使用函数对象或者lambda生成匿名函数对象 进一步泛化
-template<typename it,typename T,typename OP>
-T accumulate1(it start, it end, T init, OP op) {
-	while (start != end) {
-		init = op(init, *start);
-		start++;
-	}
-	return init;
-}
+#include <unordered_map>
+#include <string>
+#include <iostream>
+using namespace std;
 int main() {
-	vector<int> p{ 1,2,3,4 };
-	int sum = 0;
-	//cout << accumulate1(p.begin(), p.end(), sum);
-	cout << accumulate1(p.begin(), p.end(), sum, [](int a, int b) {return a + b; });
-	
+	unordered_map< string,int> words;
+	for (string s; cin >> s;) ++words[s];
+	for (const auto& p : words) cout << p.first << " " << p.second << endl;
 }
