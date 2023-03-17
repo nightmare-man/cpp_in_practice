2ee843d991314e2008bcd1fcfb1b02d9562f8267
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-17 22:32:22 +0800

    fltk install and arigithm

--- Project13/main.cpp
+++ Project13/main.cpp
@@ -1,17 +1,34 @@
 #include "../include/std_lib_facilities.h"
-int main() {
-	string from, to;
-	cin >> from >> to;
-	ifstream in{ from };
-	ofstream out{ to };
-	if (!in || !out) error("can't open file");
-	vector<string> v{ istream_iterator<string>{in},istream_iterator<string>{} };
-	for (auto& p : v) {
-		for (auto& x : p) {
-			x = tolower(x);
+
+int solution(const string& s) {
+	if (s.size() == 0) return 0;
+	int sz = s.size();
+	vector<vector<int>> p(sz, vector<int>(sz));
+	//左边界
+	for (int i = 0; i < sz; i++) {
+		//右边界
+		for (int j = i; j < sz; j++) {
+			if (i == j) {
+				p[i][j] = 1;
+			}
+			else {
+				auto end = s.begin() + j;
+				auto ret = find(s.begin() + i, end, s[j]);
+				if (ret == end) p[i][j] = p[i][j - 1] + 1; else p[i][j] = p[i][j - 1];
+			}
 		}
 	}
-	ostream_iterator<string> oo{ out," " };
-	copy(v.begin(), v.end(), oo);
-	return 0;
+
+	int ret = 1;
+	for (int j = 1; j < sz; j++) {
+		int x = ret;
+		for (int i = 0; i <= j; i++) {
+			x += p[i][j];
+		}
+		ret = x;
+	}
+	return ret;
+}
+int main() {
+	cout << solution("aaa");
 }
