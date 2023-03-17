735da6087a49687c68d5cab98d645e3bd98e563a
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-17 10:36:03 +0800

    list.sort

--- Project13/main.cpp
+++ Project13/main.cpp
@@ -3,6 +3,10 @@ struct Item {
 	string name;
 	int iid;
 	double value;
+	//类内重载
+	bool operator<(const Item&a);
+	//也可以用friendly 类外重载，这样需要传
+	//两个参数，因为不再默认传入this指针
 };
 istream& operator>>(istream& in,Item& v) {
 	in >> v.name >> v.iid >> v.value;
@@ -13,27 +17,22 @@ ostream& operator<<(ostream& os, const Item& v) {
 	os << "{" << v.name << "," << v.iid << "," << v.value << "}\n";
 	return os;
 }
+bool Item::operator<(const Item&a) {
+	return name > a.name;
+}
 int main() {
 	string from;
 	cin >> from;
-	if (!cin)error( "no input" );
+	if (!cin) error("not input filename\n");
 	ifstream is{ from };
-	vector<Item> x;
-	if (!is) error("open file error");
-	while (true) {
-		Item p;
-		is >> p;
-		if (!is) break;
-		x.push_back(p);
-	}
-	x.insert(x.begin(), Item{ "horse shoe",99,12.34 });
-	x.insert(x.begin(), Item{ "Canon S400",9988,499.95 });
-	x.erase(find_if(x.begin(), x.end(), [](Item i) {return i.name == "Canon S400"; }));
-	sort(x.begin(), x.end(), [](const Item& a, const Item& b) {
-		return a.value > b.value;
-		});
-	for (auto p : x) {
-		cout << p;
-	}
-	return 0;
-}
+	if (!is) error("can't open file");
+	istream_iterator<Item> begin{ is };
+	istream_iterator<Item> end{};
+	//直接使用流迭代器初始化容器，相当于一直从输入流读Item
+	list<Item> lll{ begin,end };
+	//sort需要支持[]的随机访问迭代器，list不提供
+	//sort(lll.begin(), lll.end());
+	//使用list.sort需要重载运算符> <
+	lll.sort();
+	for (auto x : lll) cout<<x;
+} 
