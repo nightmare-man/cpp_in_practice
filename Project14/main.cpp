c6f7ec032af141a1405ff358d3b224b8fc4f16ce
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-18 15:49:38 +0800

    导入gui接口类，并且对接fltk

--- Project14/main.cpp
+++ Project14/main.cpp
@@ -1,12 +1,5 @@
-#include <FL/Fl.h>
-#include <FL/Fl_Box.h>
-#include <FL/Fl_Window.h>
+#include "../include/Simple_window.h"
+#include "../include/Graph.h"
 int main() {
-	Fl_Window window(200, 200, "Window titile");
-	Fl_Box box(0, 0, 200, 200, "hey i am here");
-	window.show();
-	return Fl::run();
+
 }
-
-
-
