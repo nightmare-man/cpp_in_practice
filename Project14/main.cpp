7664b20bce8f2aaa599085d0a260963a8f353a06
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-18 10:34:15 +0800

    导入fltk库

--- Project14/main.cpp
+++ Project14/main.cpp
@@ -1,6 +1,6 @@
-#include <Fl.h>
-#include <Fl_Box.h>
-#include <Fl_Window.h>
+#include <FL/Fl.h>
+#include <FL/Fl_Box.h>
+#include <FL/Fl_Window.h>
 int main() {
 	Fl_Window window(200, 200, "Window titile");
 	Fl_Box box(0, 0, 200, 200, "hey i am here");
