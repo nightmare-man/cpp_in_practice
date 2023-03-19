05b4364ce175b018e56e477fd31b595c4adc4fc7
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-19 11:44:44 +0800

    Arc类实现及继承的三种方式

--- Project14/main.cpp
+++ Project14/main.cpp
@@ -1,16 +1,38 @@
 #include "../include/Simple_window.h"
 #include "../include/Graph.h"
-
-int main() {
-	using namespace Graph_lib;
-	Simple_window win{ Point{200,200},500,500,"Color Style" };
-	Vector_ref<Graph_lib::Rectangle> vr;
-	for (int i = 0; i < 16; i++) {
-		for (int j = 0; j < 16; j++) {
-			vr.push_back(new Graph_lib::Rectangle{ Point{i * 20,j * 20},20,20 });
-			vr[vr.size() - 1].set_fill_color(Color(i * 16 + j));
-			win.attach(vr[vr.size() - 1]);
+namespace Graph_lib {
+	//三种继承方式 public private protected
+	//继承的默认规则，private
+	//派生类不能访问 基类中的private成员，但可以访问public protected成员
+	//但派生类继承的基类的public protected成员 也变为private
+	//即不能 用"派生类的对象"访问基类的public protected成员
+	
+	//如果这里不加public 那么默认private继承，那么win.attach(arc)就报错，因为无法用arc来访问基类的public protected对象了，导致attach内部无法工作
+	//因此需要用public修饰
+	class Arc :public Shape {
+	public:
+		Arc(Point p1, int w1, int h1, double s1, double e1) :w{ w1 }, h{ h1 }, s{ s1 }, e{ e1 } {
+			add(p1);
+		}
+		void draw_lines()const;
+	private:
+		int w;
+		int h;
+		double s;
+		double e;
+	};
+	void Arc::draw_lines()const {
+		if (color().visibility()) {
+			fl_color(color().as_int());
+			fl_arc(point(0).x, point(0).y, w, h, s, e);
 		}
 	}
+}
+
+int main() {
+	Simple_window win{ Point{100,100},500,500,"window" };
+	Graph_lib::Arc arc{ Point{100,100},50,50,0,90 };
+	arc.set_color(Color::red);
+	win.attach(arc);
 	win.wait_for_button();
 }
