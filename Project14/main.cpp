0ad1982b2e8bd649bc515d2e873f4803b8561eac
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-20 12:38:58 +0800

    虚函数与虚函数表

--- Project14/main.cpp
+++ Project14/main.cpp
@@ -5,39 +5,56 @@
 #else
 #include "../include/std_lib_facilities.h"
 #endif
+//虚函数实现原理
+//基类成员实际有一个函数指针vptr 指向一个虚函数表
+//基类中所有虚函数的地址都在该表中
+//派生类由于继承了基类，因此也获得了基类成员(包括vptr)
+//但是其实例的vptr指向的是自己的虚函数表
+//派生类的虚函数表中，没有覆盖的虚函数与基类的一样
+//覆盖了的就是对应的覆写函数的地址
 class B1 {
 public:
-	virtual void vf() {
-		cout << "B1::vf\n";
+	virtual void vf1() {
+		cout << "B1::vf1\n";
+	};
+	virtual void vf2() {
+		cout << "B1::vf2\n";
 	};
 	void f() {
 		cout << "B1::f\n";
-	};
+	}
 };
 class D1 :public B1 {
 public:
-	void vf()override {
-		cout << "D1::vf\n";
+	void vf1()override {
+		cout << "D1::vf1\n";
 	};
 	void f() {
 		cout << "D1::f\n";
 	}
 };
+class D2 :public B1 {
+	void vf2() override{
+		cout << "D2::vf2\n";
+	}
+	void f() {
+		cout << "D2::f\n";
+	}
+};
 int main() {
-	//纯虚函数才是抽象类 无法实例化
-	//这儿只有一个虚函数
 	B1 b;
-	b.vf();
-	b.f();
-	
-	D1 d;
-	d.vf();
-	d.f();//D1::f
+	D1 d1;
+	D2 d2;
+	b.vf1();//b作为基类的实例化，vptr指向基类的虚函数表
+	b.vf2();//基类的虚函数表里都记录的都是基类的虚函数地址
 
-	B1& b1 = d;
-	//实际是一个d因此调用d对vf的重载
-	//引用实际是一个指针常量
-	b1.vf(); // 除了虚函数或按照其实际派生类型
-	//来执行，其余函数都看其按照什么类型解释
-	b1.f();//D1::f  //错了这儿是B1::f
+	d1.vf1();//d1作为派生类D1的实例化，vptr指向派生类的虚函数表
+	d1.vf2();//D1的虚函数表里vf1的地址覆写为D1自己的 vf2的没被覆写
+
+	B1& ref = d1;//ref是d1的引用，但被解释为B1,因此被截断，只剩下
+	//基类成员 包括vptr
+	ref.vf1();//ref作为派生类D1实例化后截断的结果，vptr仍然指向D1
+	//的虚函数表，因此虚函数的绑定和D1的派生类一样
+	ref.f();//但是非虚函数则是从派生类中调用同名同参数的。
+	//因此会输出B1::f
 }
