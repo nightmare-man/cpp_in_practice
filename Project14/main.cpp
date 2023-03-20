49c5af7806c84e384f195403b1f5e9a38d691e00
nightmareman <107lsmlsm@whut.edu.cn>
2023-03-20 15:54:40 +0800

    practice3

--- Project14/main.cpp
+++ Project14/main.cpp
@@ -1,20 +1,27 @@
+#define Graph
 #ifdef Graph
 #include "../include/Simple_window.h"
 #include "../include/Graph.h"
-using namespace Graph_lib;
 #else
 #include "../include/std_lib_facilities.h"
 #endif
-class B2 {
-public:
-	//纯虚函数
-	virtual void pvf() = 0;
-};
-class B3 {
-protected:
-	B3();
-};
+namespace Graph_lib {
+	class Immobile_Cicle :public Circle {
+	public:
+		Immobile_Cicle(const Point& p, int r) :Circle(p, r) {};
+		void move(int dx,int dy)override {
+			return;
+		}
+	};
+}
 int main() {
-	B2 b;//不允许定义抽象类的实例
-	B3 b;
+	using namespace Graph_lib;
+	Simple_window win{ Point{100,100},500,500,"window" };
+	Immobile_Cicle cl{ Point{100,100},50 };
+	cl.set_color(Color::red);
+	win.attach(cl);
+	win.wait_for_button();
+	cl.move(100, 0);
+	win.wait_for_button();
+	
 }
